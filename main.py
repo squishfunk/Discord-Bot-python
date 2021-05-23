b1ba55fbf568ca69230815e6f00d79e11f132121
Damian <kakaka123321@wp.pl>
2021-05-24 01:09:34 +0200

    Change token and add environment variable for it

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('Njk0OTMwMDQ1NjQyMjExMzk4.XoSyCg.leIXE0H42Vf_YjoVCOoT5_5yZNA')
+client.run(os.getenv('TOKEN'))
